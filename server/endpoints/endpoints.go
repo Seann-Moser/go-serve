a2d50f211097ee2c0afee4bdb6990572c3b5553b
Seann Moser <67982353+Seann-Moser@users.noreply.github.com>
2022-08-16 09:33:11 -0700

    fix: subdomains

--- server/endpoints/endpoints.go
+++ server/endpoints/endpoints.go
@@ -17,8 +17,8 @@ type EndpointHandler func(w http.ResponseWriter, r *http.Request)
 type Endpoint struct {
 	SubDomain       string          `json:"sub_domain" db:"sub_domain" q_config:"primary,where:="`
 	Redirect        string          `json:"redirect" db:"redirect" q_config:"join,update"`
-	URLPath         string          `json:"url_path" yaml:"url_path" db:"url_path" q_config:"primary,delete,join,update,where:="`
-	PermissionLevel Permission      `json:"permission_level" yaml:"permission_level" db:"permission_level" q_config:"primary,delete,join,update,where:<="`
+	URLPath         string          `json:"url_path" yaml:"url_path" db:"url_path" q_config:"primary,data_type:text,delete,join,update,where:="`
+	PermissionLevel Permission      `json:"permission_level" yaml:"permission_level" db:"permission_level" q_config:"join,update,where:<="`
 	Methods         []string        `json:"methods" yaml:"methods" db:"-"`
 	HandlerFunc     EndpointHandler `db:"-" json:"-"`
 	Handler         http.Handler    `db:"-" json:"-"`
