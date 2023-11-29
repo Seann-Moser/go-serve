c951af0a6169374a2caa861f19e4162a45a75df5
Seann-Moser <seannmoser@gmail.com>
2023-11-28 17:30:52 -0800

    fix: update header values

--- pkg/clientpkg/helper.go
+++ pkg/clientpkg/helper.go
@@ -319,6 +319,32 @@ func createDir(dir string) error {
 	return nil
 }
 
+func snakeCaseToHeader(inputUnderScoreStr string) (camelCase string) {
+	//snake_case to camelCase
+
+	isToUpper := false
+
+	for k, v := range inputUnderScoreStr {
+		if k == 0 {
+			camelCase = strings.ToUpper(string(inputUnderScoreStr[0]))
+		} else {
+			if isToUpper {
+				camelCase += strings.ToUpper(string(v))
+				isToUpper = false
+			} else {
+				if v == '_' {
+					isToUpper = true
+					camelCase += "-"
+				} else {
+					camelCase += string(v)
+				}
+			}
+		}
+	}
+	return
+
+}
+
 func snakeCaseToCamelCase(inputUnderScoreStr string) (camelCase string) {
 	//snake_case to camelCase
 
