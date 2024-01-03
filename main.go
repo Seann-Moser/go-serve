aff5baedce1f9426525c879d35956f54af2a7e06
Seann-Moser <seannmoser@gmail.com>
2024-01-03 12:40:33 -0800

    chore: support swagger

--- main.go
+++ main.go
@@ -12,6 +12,24 @@ import (
 	"github.com/Seann-Moser/go-serve/server/handlers"
 )
 
+// @title 
+// @version 
+// @description 
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @schemes http https
+// @host 
+// @BasePath /
+// @query.collection.format multi
+
+
+
 func main() {
 	s := server.NewServer(context.Background(), "8888", "/test", 0, false)
 	if err := s.AddEndpoints(context.Background(), handlers.HealthCheck); err != nil {
